8928917534d60ae17371aba60f430d3f03ef12b1
sudh33ra <dulansda@gmail.com>
2020-05-09 23:41:45 +0530

    Enabling plugins * removing update nags

--- core/site/wp-content/themes/enfold/functions.php
+++ core/site/wp-content/themes/enfold/functions.php
@@ -557,6 +557,12 @@ if ( ! function_exists( '_wp_render_title_tag' ) )
 
 require_once( 'functions-enfold.php');
 
+function remove_core_updates(){
+global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+}
+add_filter('pre_site_transient_update_core','remove_core_updates');
+add_filter('pre_site_transient_update_plugins','remove_core_updates');
+add_filter('pre_site_transient_update_themes','remove_core_updates');
 
 /*
  * add option to edit elements via css class
